72b97d5efa1da763bbdfdab625a851de0b76de44
Atanu Paul <atanu.paul.rims@gmail.com>
2021-02-20 14:05:16 +0530

    pushing changes

---
+++ advanceNodeCodes/threads.js
@@ -0,0 +1,24 @@
+// a small program to check if node.js is really single threaded or not.
+const crypto = require("crypto");
+
+const start = Date.now();
+
+crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+  console.log("1:", Date.now() - start);
+});
+
+crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+  console.log("2:", Date.now() - start);
+});
+
+crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+  console.log("3:", Date.now() - start);
+});
+
+crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+  console.log("4:", Date.now() - start);
+});
+
+crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
+  console.log("5:", Date.now() - start);
+});
